896c059491e4ba206876f828821640ad2917a201
Connor McCue <connor.mccue@comcast.net>
2022-07-28 00:26:33 -0700

    the character selector works

--- app.js
+++ app.js
@@ -5,66 +5,78 @@ const bottomDropdown = document.getElementById('bottom-dropdown');
 const headEl = document.getElementById('head');
 const middleEl = document.getElementById('middle');
 const bottomEl = document.getElementById('bottom');
+const headImgTag = document.getElementById('head-img');
+const middleImgTag = document.getElementById('middle-img');
+const bottomImgTag = document.getElementById('bottom-img');
 const reportEl = document.getElementById('report');
 const catchphrasesEl = document.getElementById('catchphrases');
 const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
 
 // set state for how many times the user changes the head, middle, and bottom
-const timesChanged = document.getElementById('time-changed');
+const timesChanged = document.getElementById('stats');
 let changeHead = 0;
 let changeMiddle = 0;
 let changeBottom = 0;
+
 // set state for all of the character's catchphrases
 let catchphrases = [];
 const catchphraseEl = document.getElementById('catchphrases');
 
 headDropdown.addEventListener('change', () => {
+    console.log(headDropdown.value);
     // get the value of the head dropdown
     // increment the head change count state
-    
+    changeHead++;
     // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    headImgTag.src = `./assets/${headDropdown.value}-head.png`;
+    
+    //headImgTag.style.backgroundImage = `./assets/duck-head.png`;
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 
 middleDropdown.addEventListener('change', () => {
     // get the value of the middle dropdown
-
+    console.log(middleDropdown.value);
     // increment the middle change count state
-    
+    changeMiddle++;
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    middleImgTag.src = `assets/${middleDropdown.value}-middle.png`;
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 
 bottomDropdown.addEventListener('change', () => {
     // get the value of the bottom dropdown
-
+    console.log(bottomDropdown.value);
     // increment the bottom change count state
-    
+    changeBottom++;
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    bottomImgTag.src = `assets/${bottomDropdown.value}-pants.png`;
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 catchphraseButton.addEventListener('click', () => {
     // get the value of the catchphrase input
-    const newCatch = catchphraseInput;
-    catchphrases.push(newCatch);
+    const newCatch = catchphraseEl;
     // push the new catchphrase to the catchphrase array in state
-
+    catchphrases.push(newCatch);
     // clear out the form input's value so it's empty to the user
-   
+    displayCatchphrases();
     // update the dom to show the new catchphrases (refactor to/call displayCatchphrases to do this work)
-
+    catchphraseEl.value = '';
 });
-
+ 
 function displayStats() {
     // text content of the reportEl to tell the user how many times they've changed each piece of the state
+    console.log(changeHead);
+    timesChanged.textContent = `you changed the head ${changeHead} times, the middle ${changeMiddle} times, the bottom ${changeBottom} times.`;
 }
+
 
 function displayCatchphrases() {
     // clear out the DOM for the currently displayed catchphrases    
@@ -77,9 +89,11 @@ function displayCatchphrases() {
     
     // create an HTML element with the catchphrase as its text content
         catchphraseEl.textContent = catchphrase;
-        catchphraseEl.classList.add('slogan');
+        catchphraseEl.classList.add('catchphrase');
         // and append that HTML element to the cleared-out DOM
-        catchphraseEl.append(catchphraseEl);
+        // catchphraseEl.append(catchphraseEl);
 
     }
 }
+
+displayCatchphrases();
